822368340c04cce4b74a14bce5314f2cfe67e98f
eliassalom <elias.29@hotmail.com>
2024-10-25 13:13:53 +0300

    feat: implement mongo url

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {MongooseModule} from "@nestjs/mongoose";
+import { UserModule } from './user/user.module';
+import { AlbumModule } from './album/album.module';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://test:123@cluster0.id10m.mongodb.net/?retryWrites=true&w=majority'), UserModule, AlbumModule],
   controllers: [AppController],
   providers: [AppService],
 })
